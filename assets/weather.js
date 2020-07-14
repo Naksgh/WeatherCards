1fd3cc2715c117deb1617aabd852ee31cd54c309
Igor Kalaciov <igorkk@live.ru>
2020-07-14 16:43:15 +0000

    wired ajax GET request to search field with error handling

--- assets/weather.js
+++ assets/weather.js
@@ -1,3 +1,24 @@
-$(document).ready(function() {
-    console.log("helo");
+$(document).ready(function(){
+
+    $("#search-button").click(function(){
+
+        var searchedLocation = $("input[name=search-field]").val();
+
+        $.ajax({
+            type: "GET",
+            url: "https://api.openweathermap.org/data/2.5/weather?q=" + searchedLocation + "&units=metric&APPID=4cec047c557bff34558b1d7ae6547462",
+            success: function(data){
+                console.log(data);
+            },
+            error: function(error){
+                console.log(error);
+                var errorMsg = error.responseJSON.message;
+                alert(`Woops, something went wrong... ${errorMsg}`);
+            }
+        });
+
+
+
+    });
+
 });
